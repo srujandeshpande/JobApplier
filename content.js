a0057fec5adaad3b157799770e5b71855bbe2c4c
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-11-04 20:35:51 +0530

    Create files and set elements

---
+++ content.js
@@ -0,0 +1,8 @@
+const {userData} = require("./data")
+
+firstNameField = document.getElementById("textInput.nameComponent--uid45-input");
+lastNameField = document.getElementById("textInput.nameComponent--uid46-input");
+addressField = document.getElementById("textInput.addressComponentsDeferred[i]--uid55-input");
+cityField = document.getElementById("textInput.addressComponentsDeferred[i]--uid56-input");
+postalCodeField = document.getElementById("textInput.addressComponentsDeferred[i]--uid57-input");
+phoneNumberField = document.getElementById("textInput.phone--uid63-input");
